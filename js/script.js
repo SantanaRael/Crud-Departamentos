56ee5097898184299dc567b19c2b2db5f7fc8c6c
Israel Santana <73674173+SantanaRael@users.noreply.github.com>
2022-09-28 11:57:28 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 let consultar = document.querySelector(".botoes-dep").children[0];
 let formu = document.querySelector(".formulario");
 let incluir = document.querySelector(".botoes-dep").children[1];
-let inputs = document.querySelector(".inputs");
 let btn_add = document.querySelector("#add");
 let table = document.querySelector("table")
 let alterar = document.querySelector(".botoes-dep").children[2];
 let add = document.querySelector("#add")
-let btn_edit = document.createElement("input")
-let btn_delete = document.createElement("input")
 let excluir = document.querySelector(".botoes-dep").children[3];
 let closeAdd = document.querySelector(".incluir")
+let btn_confirm = document.querySelector(".confirm")
+
+let btn_edit = document.createElement("input")
+let btn_delete = document.createElement("input")
 
 
 
 
 consultar.addEventListener("click", function mostrarForm() {
-    formu.classList.toggle("mostrarForm");
+    formu.classList="formulario mostrarForm"
+    btn_confirm.classList="confirm";
     if(document.querySelector(".mostrarIncluir")){
         closeAdd.classList="incluir"
     }
@@ -27,6 +29,7 @@ consultar.addEventListener("click", function mostrarForm() {
 incluir.addEventListener(
   "click",
   function mostrarInputs() {
+      
 
     if(formu.classList == "formulario mostrarForm"){
         inputs.classList.toggle("mostrarInput");
@@ -43,13 +46,25 @@ btn_add.addEventListener("click", function(){
     let tr = document.createElement("tr")
     let td1 = document.createElement("td")
     let td2 = document.createElement("td")
+    let text_input1 = document.createElement("input")
+    let text_input2 = document.createElement("input")
 
     
-    td1.innerHTML= cod
-    td2.innerHTML= desc.toUpperCase()
+    text_input1.classList="inputs"
+    text_input1.type="text"
+    text_input1.value=`${cod}`
+    text_input1.disabled="true"
+
+    text_input2.classList="inputs"
+    text_input2.type="text"
+    text_input2.value=`${desc.toUpperCase()}`
+    text_input2.disabled="true"
 
     table.appendChild(tr)
     tr.appendChild(td1)
+    td1.appendChild(text_input1)
+    td2.appendChild(text_input2)
+
     tr.appendChild(td2)
     alert("Item Adicionado!")
 
@@ -63,7 +78,24 @@ incluir.addEventListener("click", function(){
 })
 
 alterar.addEventListener("click", function(){
-    alert("não deu tempo :/")
+
+    if(document.querySelector(".mostrarIncluir")){
+        closeAdd.classList="incluir"
+    }
+
+    btn_confirm.classList="confirm open";
+    let inputs = document.querySelectorAll(".inputs")
+    formu.classList="formulario mostrarForm"
+    inputs.forEach((element) => (element.disabled = false));
+    
+
+})
+
+btn_confirm.addEventListener("click", function(){
+    let inputs = document.querySelectorAll(".inputs")
+
+    inputs.forEach((element) => (element.disabled = true));
+    
 })
 
 excluir.addEventListener("click", function(){
@@ -73,6 +105,8 @@ excluir.addEventListener("click", function(){
 
 
 
+
+///erro: não ta dando pra editar os novos itens, apenas os já existentes
 
 
 
